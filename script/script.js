aa0a536a1d8e08fa898341fb343ebf045ab78a44
Mbuguambuthi <mbuguambuthi24@gmail.com>
2025-06-15 19:39:28 +0300

    Readme.md
    Index, About pages done.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,23 @@
+const video = document.getElementById('video');
+const overlay = document.querySelector('.overlay');
+const playButton = document.getElementById('playButton');
 
+playButton.addEventListener('click', play);
+
+function play(){
+    if(video.play){
+        video.play();
+    overlay.style.display = 'none';
+    playButton.style.display = "none";
+
+
+    }
+
+    else{
+        video.paused();
+    overlay.style.display = 'block';
+    playButton.style.display = "block";
+
+
+}
+}
