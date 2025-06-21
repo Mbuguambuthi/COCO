506a30976f7cbe015f40a5a2ecbbceee706edb85
Mbuguambuthi <mbuguambuthi24@gmail.com>
2025-06-21 15:14:13 +0300

    Readme.md
    New updates/21/06/35.

---
+++ script/service.js
@@ -0,0 +1,11 @@
+/* VIDEO SCRIPT */
+ const myVid = document.querySelector(".instagram-media");
+
+
+ myVid.addEventListener('click', () =>{
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+ } )
